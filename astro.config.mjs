172fe3ef908eddc26412da9b32502f6981060cd1
Jangya satapathy <jangya069@gmail.com>
2026-07-15 00:48:07 +0530

    building astro site as static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://jangya.github.io',
   output: 'static',
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
